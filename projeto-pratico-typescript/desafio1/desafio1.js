0033b15eee048d8d8262354c97d07d07ad9d86f8
Caio Santos <104065569+AcidCaaio@users.noreply.github.com>
2022-08-08 19:55:46 +0200

    Add files via upload

---
+++ projeto-pratico-typescript/desafio1/desafio1.js
@@ -0,0 +1,8 @@
+var employee = {
+    code: Number(document.getElementById(input1)),
+    name: document.getElementById(input2)
+};
+
+var codigo = employee.code;
+var nome = employee.name;
+console.log("Desafio 1 \nC\u00F3digo: ".concat(codigo, " / Nome: ").concat(nome));
